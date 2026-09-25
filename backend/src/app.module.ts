c0772e18745ef466f27e877c2493416ebc31a7d4
Francisco Del`Gaudio <delgaudiofrancisco.junior@gmail.com>
2026-09-25 15:26:09 -0300

    feat: add @nestjs/config dependency and configure ConfigModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ConfigModule } from '@nestjs/config';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ObserveModule.forRoot({
       appKey: process.env.OBSERVE_APP_KEY!,
       appSecret: process.env.OBSERVE_APP_SECRET!,
@@ -16,4 +18,4 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
